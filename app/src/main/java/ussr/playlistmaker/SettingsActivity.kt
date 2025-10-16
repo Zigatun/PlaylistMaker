cb1b75531aee2eaccc1867d57ac2b84ea4b89a1e
Viktor Stavitskiy <StavitskiyVS@polyus.com>
2025-10-16 19:43:31 +0700

    implemented navigation to home page

--- app/src/main/java/ussr/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/ussr/playlistmaker/SettingsActivity.kt
@@ -1,11 +1,19 @@
 package ussr.playlistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity()  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        val backButton = findViewById<ImageView>(R.id.back_button)
+        backButton.setOnClickListener {
+            val mainIntent = Intent(this, MainActivity::class.java)
+            startActivity(mainIntent)
+        }
     }
 }
