6a40b6c8197a61b23b6ebf8f59f91f6edc82966c
Viktor Stavitskiy <StavitskiyVS@polyus.com>
2025-10-23 20:40:26 +0700

    implemented buttons logic in SettingsActivity

--- app/src/main/java/ussr/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/ussr/playlistmaker/SettingsActivity.kt
@@ -1,7 +1,9 @@
 package ussr.playlistmaker
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.FrameLayout
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,10 +12,30 @@ class SettingsActivity : AppCompatActivity()  {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val backButton = findViewById<ImageView>(R.id.back_button)
-        backButton.setOnClickListener {
+        findViewById<ImageView>(R.id.back_button).setOnClickListener {
             val mainIntent = Intent(this, MainActivity::class.java)
             startActivity(mainIntent)
+        }
+
+        findViewById<FrameLayout>(R.id.share_app).setOnClickListener{
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.type = "text/plain"
+            shareIntent.putExtra(Intent.EXTRA_TEXT, this.getString(R.string.share_link));
+            startActivity(shareIntent)
+        }
+
+        findViewById<FrameLayout>(R.id.contact_with_support).setOnClickListener {
+            val emailIntent = Intent(Intent.ACTION_SEND)
+            emailIntent.type = "text/plain"
+            emailIntent.data = Uri.parse("mailto:" + this.getString(R.string.email_support_address))
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, this.getString(R.string.email_support_title))
+            emailIntent.putExtra(Intent.EXTRA_TEXT, this.getString(R.string.email_support_body))
+            startActivity(emailIntent)
+        }
+
+        findViewById<FrameLayout>(R.id.user_agreement).setOnClickListener {
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(this.getString(R.string.user_agreement_link)))
+            startActivity(browserIntent)
         }
     }
 }
