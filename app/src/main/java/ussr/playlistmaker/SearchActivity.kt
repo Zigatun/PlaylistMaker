d320894ccdef576d4f42f961e55d6daa955b84dd
Viktor Stavitskiy <StavitskiyVS@polyus.com>
2025-10-24 20:40:43 +0700

    implemented search bar view functionality

--- app/src/main/java/ussr/playlistmaker/SearchActivity.kt
+++ app/src/main/java/ussr/playlistmaker/SearchActivity.kt
@@ -1,6 +1,13 @@
 package ussr.playlistmaker
 
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
@@ -11,9 +18,39 @@ class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
-        val toolbar = findViewById<android.widget.Toolbar>(R.id.main_toolbar)
-        toolbar.setNavigationOnClickListener{
+
+        val searchBar = findViewById<EditText>(R.id.search_bar)
+        val clearButton = findViewById<ImageView>(R.id.search_bar_clear_text)
+
+        findViewById<android.widget.Toolbar>(R.id.main_toolbar).setNavigationOnClickListener{
             finish()
         }
+
+        clearButton.setOnClickListener{
+            searchBar.setText("")
+            val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as? InputMethodManager
+            inputMethodManager?.hideSoftInputFromWindow(searchBar.windowToken, 0)
+        }
+
+        val searchBarTextWatcher = object : TextWatcher{
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+
+
+                clearButton.visibility = when(s.isNullOrEmpty()){
+                    true -> View.GONE
+                    false -> View.VISIBLE
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+        }
+
+        searchBar.addTextChangedListener(searchBarTextWatcher)
     }
 }
