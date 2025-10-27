e5513b8116fbc180e16367f5a9653e1771d107d4
Viktor Stavitskiy <StavitskiyVS@polyus.com>
2025-10-27 19:20:41 +0700

    attempt to fix bug when app may crash on sendto intent on some devices

--- app/src/main/java/ussr/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/ussr/playlistmaker/SettingsActivity.kt
@@ -19,21 +19,22 @@ class SettingsActivity : AppCompatActivity()  {
         findViewById<FrameLayout>(R.id.share_app).setOnClickListener{
             val shareIntent = Intent(Intent.ACTION_SEND)
             shareIntent.type = "text/plain"
-            shareIntent.putExtra(Intent.EXTRA_TEXT, this.getString(R.string.share_link));
+            shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_link));
             startActivity(shareIntent)
         }
 
         findViewById<FrameLayout>(R.id.contact_with_support).setOnClickListener {
-            val emailIntent = Intent(Intent.ACTION_SEND)
-            emailIntent.type = "text/plain"
-            emailIntent.data = Uri.parse("mailto:" + this.getString(R.string.email_support_address))
-            emailIntent.putExtra(Intent.EXTRA_SUBJECT, this.getString(R.string.email_support_title))
-            emailIntent.putExtra(Intent.EXTRA_TEXT, this.getString(R.string.email_support_body))
+            val emailIntent = Intent(Intent.ACTION_SENDTO)
+            emailIntent.data = Uri.parse("mailto:")
+
+            emailIntent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.email_support_address)))
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.email_support_title))
+            emailIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.email_support_body))
             startActivity(emailIntent)
         }
 
         findViewById<FrameLayout>(R.id.user_agreement).setOnClickListener {
-            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(this.getString(R.string.user_agreement_link)))
+            val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.user_agreement_link)))
             startActivity(browserIntent)
         }
     }
