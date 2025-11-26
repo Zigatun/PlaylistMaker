a8c62bc17a59d874fa3b58a5b1f0b19f93daa4ac
Viktor Stavitskiy <StavitskiyVS@polyus.com>
2025-11-26 20:15:27 +0700

    fixed practicum issues

--- app/src/main/java/ussr/playlistmaker/SearchActivity.kt
+++ app/src/main/java/ussr/playlistmaker/SearchActivity.kt
@@ -33,13 +33,6 @@ class SearchActivity : AppCompatActivity() {
 
     private val itunesSearchApiService = retrofit.create<ItunesSearchApiService>()
 
-    //    private var tracks: List<Track> = listOf(
-//        Track("Smells Like Teen Spirit", "Nirvana", "5:01", "https://is5-ssl.mzstatic.com/image/thumb/Music115/v4/7b/58/c2/7b58c21a-2b51-2bb2-e59a-9bb9b96ad8c3/00602567924166.rgb.jpg/100x100bb.jpg"),
-//        Track("Billie Jean", "Michael Jackson", "4:35", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/3d/9d/38/3d9d3811-71f0-3a0e-1ada-3004e56ff852/827969428726.jpg/100x100bb.jpg"),
-//        Track("Stayin' Alive", "Bee Gees", "4:10", "https://is4-ssl.mzstatic.com/image/thumb/Music115/v4/1f/80/1f/1f801fc1-8c0f-ea3e-d3e5-387c6619619e/16UMGIM86640.rgb.jpg/100x100bb.jpg"),
-//        Track("Whole Lotta Love", "Led Zeppelin", "5:33", "https://is2-ssl.mzstatic.com/image/thumb/Music62/v4/7e/17/e3/7e17e33f-2efa-2a36-e916-7f808576cf6b/mzm.fyigqcbs.jpg/100x100bb.jpg"),
-//        Track("Sweet Child O'Mine", "Guns N' Roses", "5:03", "https://is5-ssl.mzstatic.com/image/thumb/Music125/v4/a0/4d/c4/a04dc484-03cc-02aa-fa82-5334fcb4bc16/18UMGIM24878.rgb.jpg/100x100bb.jpg"),
-//        )
     private fun setPlaceholderMessage(message: String = "", isError: Boolean = false) {
         val placeholderView = findViewById<LinearLayout>(R.id.error_placeholder)
         val tracksView = findViewById<RecyclerView>(R.id.tracksRecyclerView)
@@ -62,7 +55,7 @@ class SearchActivity : AppCompatActivity() {
         }
     }
 
-    private fun doSearch(request: String) {
+    private fun doSearch(request: String, wasBeenCleared: Boolean = false) {
         itunesSearchApiService.search(request)
             .enqueue(object : Callback<ItunesSearchResult> {
                 override fun onResponse(
@@ -70,9 +63,13 @@ class SearchActivity : AppCompatActivity() {
                     response: Response<ItunesSearchResult?>
                 ) {
                     if (response.isSuccessful) {
+                        setPlaceholderMessage("")
+                        if(wasBeenCleared){
+                            findViewById<RecyclerView>(R.id.tracksRecyclerView).isVisible = false
+                            return
+                        }
                         val respObjects = response.body()?.results
                         if (respObjects != null && respObjects.isNotEmpty()) {
-                            setPlaceholderMessage("")
                             findViewById<RecyclerView>(R.id.tracksRecyclerView).adapter =
                                 ItunesTrackAdapter(respObjects)
                         } else {
@@ -85,7 +82,11 @@ class SearchActivity : AppCompatActivity() {
                     call: Call<ItunesSearchResult?>,
                     t: Throwable
                 ) {
-                    //setPlaceholderMessage(getString(R.string.connection_troubles) + "\n\n" + t.message.toString(), true)
+                    if(wasBeenCleared){
+                        setPlaceholderMessage("")
+                        //findViewById<LinearLayout>(R.id.error_placeholder)
+                        return
+                    }
                     setPlaceholderMessage(getString(R.string.connection_troubles) + "\n\n" + getString(R.string.trouble_no_internet), true)
                 }
 
@@ -111,7 +112,7 @@ class SearchActivity : AppCompatActivity() {
 
         clearButton.setOnClickListener {
             searchBar.setText("")
-            doSearch("")
+            doSearch("", true)
             val inputMethodManager = getSystemService(INPUT_METHOD_SERVICE) as? InputMethodManager
             inputMethodManager?.hideSoftInputFromWindow(searchBar.windowToken, 0)
         }
