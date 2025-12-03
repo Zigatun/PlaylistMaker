66083d261c552b88288c17988126befe577b6b17
Viktor Stavitskiy <StavitskiyVS@polyus.com>
2025-12-03 20:48:41 +0700

    added id field to dto

--- app/src/main/java/ussr/playlistmaker/models/ItunesTrack.kt
+++ app/src/main/java/ussr/playlistmaker/models/ItunesTrack.kt
@@ -1,3 +1,3 @@
 package ussr.playlistmaker.models
 
-data class ItunesTrack (val trackName: String, val artistName: String, val trackTimeMillis: Long, val artworkUrl100: String)
+data class ItunesTrack (val trackId: Long, val trackName: String, val artistName: String, val trackTimeMillis: Long, val artworkUrl100: String)
