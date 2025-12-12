6ea2c7cb55c4dcc0d4095a348c108179561ea885
Viktor Stavitskiy <StavitskiyVS@polyus.com>
2025-12-12 20:58:36 +0700

    added kotlin-parcelize plugin

--- app/src/main/java/ussr/playlistmaker/models/ItunesTrack.kt
+++ app/src/main/java/ussr/playlistmaker/models/ItunesTrack.kt
@@ -1,3 +1,5 @@
 package ussr.playlistmaker.models
-
-data class ItunesTrack (val trackId: Long, val trackName: String, val artistName: String, val trackTimeMillis: Long, val artworkUrl100: String)
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+@Parcelize
+data class ItunesTrack (val trackId: Long, val trackName: String, val artistName: String, val trackTimeMillis: Long, val artworkUrl100: String): Parcelable
