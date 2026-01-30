dbc68809f661908b17ab828235757925525ca0bf
Zigatun <zigatun@icloud.com>
2026-01-30 19:20:31 +0700

    implemented preview playback from itunes

--- app/src/main/java/ussr/playlistmaker/PlayerActivity.kt
+++ app/src/main/java/ussr/playlistmaker/PlayerActivity.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.util.DisplayMetrics
 import android.util.TypedValue
 import android.widget.Button
+import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -22,7 +23,7 @@ import ussr.playlistmaker.models.ItunesTrack
 
 class PlayerActivity : AppCompatActivity() {
     private var mediaPlayer = MediaPlayer()
-    private lateinit var playButton: Button
+    private lateinit var playButton: ImageButton
     @SuppressLint("SetTextI18n")
     @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,11 +42,20 @@ class PlayerActivity : AppCompatActivity() {
         findViewById<android.widget.Toolbar>(R.id.main_toolbar).setNavigationOnClickListener {
             finish()
         }
-
+        playButton.setOnClickListener {
+            when(playerState) {
+                PLAYER_STATE_PLAYING -> {
+                    pausePlayer()
+                }
+                PLAYER_STATE_PREPARED, PLAYER_STATE_PAUSED -> {
+                    startPlayer()
+                }
+            }
+        }
         val track = intent.getParcelableExtra("track", ItunesTrack::class.java)
         track?.let{
+            preparePlayer(track.previewUrl)
             findViewById<TextView>(R.id.track_play_position).text = "0:00"
-
             findViewById<TextView>(R.id.track_name).text = it.trackName
             findViewById<TextView>(R.id.track_author).text = it.artistName
             findViewById<TextView>(R.id.duration).text = it.getHumanizedTime()
@@ -76,4 +86,47 @@ class PlayerActivity : AppCompatActivity() {
                 .into(albumImage)
         }
     }
+
+    override fun onPause() {
+        super.onPause()
+        pausePlayer()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.release()
+    }
+
+    private fun preparePlayer(url:String) {
+        mediaPlayer.setDataSource(url)
+        mediaPlayer.prepareAsync()
+        playButton.isEnabled = true
+        mediaPlayer.setOnPreparedListener {
+            playButton.setImageResource(R.drawable.play_icon)
+            playerState = PLAYER_STATE_PREPARED
+        }
+        mediaPlayer.setOnCompletionListener {
+            playButton.setImageResource(R.drawable.pause_icon)
+            playerState = PLAYER_STATE_PREPARED
+        }
+    }
+    private fun startPlayer() {
+        playButton.setImageResource(R.drawable.pause_icon)
+        mediaPlayer.start()
+        playerState = PLAYER_STATE_PLAYING
+    }
+
+    private fun pausePlayer() {
+        playButton.setImageResource(R.drawable.play_icon)
+        mediaPlayer.pause()
+        playerState = PLAYER_STATE_PAUSED
+    }
+    companion object {
+        private const val PLAYER_STATE_DEFAULT = 0
+        private const val PLAYER_STATE_PREPARED = 1
+        private const val PLAYER_STATE_PLAYING = 2
+        private const val PLAYER_STATE_PAUSED = 3
+    }
+
+    private var playerState = PLAYER_STATE_DEFAULT
 }
