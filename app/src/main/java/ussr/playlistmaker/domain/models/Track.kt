47278980e5a3f42d2cc5d24710d1f0d1bf8cc8d9
Viktor Stavitskiy <StavitskiyVS@polyus.com>
2026-02-10 19:58:27 +0700

    fixed missed field

--- app/src/main/java/ussr/playlistmaker/domain/models/Track.kt
+++ app/src/main/java/ussr/playlistmaker/domain/models/Track.kt
@@ -1,15 +1,11 @@
 package ussr.playlistmaker.domain.models
 
-import ussr.playlistmaker.data.dto.TrackDto
-import java.text.SimpleDateFormat
-import java.time.ZoneId
-import java.util.Locale
-
 class Track (val trackId: Long,
                      val trackName: String,
                      val artistName: String,
                      val trackTime: String,
                      val artworkUrl: String,
+                     val coverArtworkUrl: String,
                      val previewUrl: String,
                      val collectionName: String?,
                      val yearOfRelease: String?,
