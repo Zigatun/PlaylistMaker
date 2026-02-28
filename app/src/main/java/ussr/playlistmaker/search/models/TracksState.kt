ca180c91c402cc896bc2ee076a61d4d9f6f69f6d
Viktor Stavitskiy <StavitskiyVS@polyus.com>
2026-03-01 01:30:48 +0700

    added state for history variant

--- app/src/main/java/ussr/playlistmaker/search/models/TracksState.kt
+++ app/src/main/java/ussr/playlistmaker/search/models/TracksState.kt
@@ -2,7 +2,7 @@ package ussr.playlistmaker.search.models
 
 sealed interface TracksState {
     object Loading : TracksState
-    data class Content(val tracks: List<Track>) : TracksState
+    data class Content(val tracks: List<Track>, val isHistory: Boolean) : TracksState
     data class Error(val message: String) : TracksState
     data class Empty(val message: String): TracksState
 }
