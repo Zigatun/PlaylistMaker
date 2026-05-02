9cc5e403e86c5128e6e31465190b3fb9f4560445
Viktor Stavitskiy <StavitskiyVS@polyus.com>
2026-05-02 15:01:50 +0700

    implemented favorites db

---
+++ app/src/main/java/ussr/playlistmaker/media/data/entity/FavoriteTrackEntity.kt
@@ -0,0 +1,20 @@
+package ussr.playlistmaker.media.data.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import java.time.Instant
+
+@Entity(tableName = "favorites")
+data class FavoriteTrackEntity(
+                               @PrimaryKey val trackId: Long,
+                               val artworkUrl: String,
+                               val coverArtworkUrl: String,
+                               val trackName: String,
+                               val artistName: String,
+                               val collectionName: String?,
+                               val releaseDate: String?,
+                               val genreName:String,
+                               val country:String,
+                               val trackTime: String,
+                               val previewUrl: String
+)
