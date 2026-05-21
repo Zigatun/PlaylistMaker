1165d801d3c529a6ada90ec28b0711f83811609b
Viktor Stavitskiy <StavitskiyVS@polyus.com>
2026-05-21 19:27:52 +0700

    fixed aspect ratio mismatch

--- app/src/main/java/ussr/playlistmaker/playlist/ui/PlaylistCreatorFragment.kt
+++ app/src/main/java/ussr/playlistmaker/playlist/ui/PlaylistCreatorFragment.kt
@@ -13,15 +13,14 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
-import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
-import com.bumptech.glide.request.RequestOptions
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.markodevcic.peko.PermissionRequester
 import com.markodevcic.peko.allGranted
@@ -66,8 +65,10 @@ class PlaylistCreatorFragment: Fragment() {
             Glide.with(this)
                 .load(state.albumPhotoUri)
                 .placeholder(R.drawable.album_art_pick_icon)
-                .centerCrop()
-                .apply(RequestOptions.bitmapTransform(RoundedCorners(radiusPx.toInt())))
+                .transform(
+                    CenterCrop(),
+                    RoundedCorners(radiusPx.toInt())
+                )
                 .into(binding.playlistImage)
         }
 
